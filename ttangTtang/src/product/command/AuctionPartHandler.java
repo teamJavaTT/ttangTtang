bcd7e922175ca29b87263cb79cb208137a013a1c
JangSeungmin <96tmdals@gmail.com>
2021-06-23 17:06:20 +0900

    0623 옥션 핸들러

---
+++ ttangTtang/src/product/command/AuctionPartHandler.java
@@ -0,0 +1,27 @@
+package product.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import member.service.User;
+import mvc.command.CommandHandler;
+import product.service.ProductService;
+
+public class AuctionPartHandler implements CommandHandler {
+	private ProductService productService = new ProductService();
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		HttpSession session = req.getSession(false);
+		User user = (User) session.getAttribute("memberUser");
+		
+		String aucIno = req.getParameter("aucIno");
+		String oPrice = req.getParameter("oPrice");
+		
+		productService.auctionPartInsert(user.getUserid(), aucIno, oPrice);
+
+		return null;
+	}
+
+}
