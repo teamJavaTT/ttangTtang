a1f76acf36d5a4b5ac4f978429684290e8d32a93
salswl127 <salswl127@gmail.com>
2021-07-20 16:13:57 +0900

    0720 로그인 필터

---
+++ ttangTtangS/src/main/java/org/zerock/interceptor/AuthInterceptor.java
@@ -0,0 +1,24 @@
+package org.zerock.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("memberUser") == null) {
+			response.sendRedirect("/member/login");
+			return false;
+		}
+		return true;
+	}
+
+}
